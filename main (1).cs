1d4b209e5ed3da9409fb73194fbe358d5314795c
Antifeev <154594298+Antifeev@users.noreply.github.com>
2023-12-22 17:15:44 +0400

    Add files via upload

---
+++ main (1).cs
@@ -0,0 +1,32 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+
+using System;
+
+namespace FactorialCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int result = CalculateFactorial(number);
+            Console.WriteLine($"Факториал числа {number} равен {result}");
+        }
+
+        static int CalculateFactorial(int n)
+        {
+            if (n == 0)
+                return 1;
+            else
+                return n * CalculateFactorial(n - 1);
+        }
+    }
+}
